417b02824d9f01487bc67979307308e9fc62bf21
Albina Zakirova <zak1804@gmail.com>
2018-10-21 03:06:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ function logWhisper(string)
 }
 function sayHiToGrandma(string)
 {
-  if(string = string.toLowerCase())
+  if (string === string.toLowerCase())
  { return "I can\'t hear you!"}
 
-  string = string.toUpperCase()
-  return "YES INDEED!"
+  if (string === string.toUpperCase())
+ { return "YES INDEED!"}
+  
+  if (string === '')
   
 }
